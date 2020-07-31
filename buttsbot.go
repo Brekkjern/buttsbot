d863687fc743503e35316aa57a79ac6388175cc6
Brekkjern <gh@brekkjern.net>
2020-07-31 16:33:30 +0200

    Added considered harmful trigger

--- buttsbot.go
+++ buttsbot.go
@@ -10,6 +10,8 @@ import (
 )
 
 func main() {
+	log.Println("Initializing buttsbot...")
+
 	nick := flag.String("nick", "buttsbot", "IRC nickname")
 	connectionString := flag.String("server", "", "Connection string to IRC network")
 	channels := flag.String("channels", "", "IRC channels to connect to.")
@@ -17,19 +19,30 @@ func main() {
 	flag.StringVar(&password, "password", "", "Password for nickserv")
 	flag.Parse()
 
+	bitcoinRegex := regexp.MustCompile("(?mi)bitcoin")
 	var buttTrigger = hbot.Trigger{
 		func(b *hbot.Bot, m *hbot.Message) bool {
 			if m.From == b.Nick {
 				return false
 			}
-			matched, err := regexp.MatchString("bitcoin", m.Content)
-			if err != nil {
-				log.Fatal(err)
-			}
-			return matched
+			return bitcoinRegex.MatchString(m.Content)
 		},
 		func(b *hbot.Bot, m *hbot.Message) bool {
 			b.Reply(m, "More like buttcoin, am I rite!?")
+			return false
+		},
+	}
+
+	harmfulRegex := regexp.MustCompile("(?mi)considered harmful")
+	var harmfulTrigger = hbot.Trigger{
+		func(b *hbot.Bot, m *hbot.Message) bool {
+			if m.From == b.Nick {
+				return false
+			}
+			return harmfulRegex.MatchString(m.Content)
+		},
+		func(b *hbot.Bot, m *hbot.Message) bool {
+			b.Reply(m, "Your FACE is considered harmful!")
 			return false
 		},
 	}
@@ -41,12 +54,16 @@ func main() {
 			bot.SASL = true
 			bot.Password = password
 		}
+		bot.HijackSession = true
 	}
+	log.Println("Initializing bot system...")
 	mybot, err := hbot.NewBot(*connectionString, *nick, options)
 	if err != nil {
 		panic(err)
 	}
-
+	log.Println("Adding triggers...")
 	mybot.AddTrigger(buttTrigger)
+	mybot.AddTrigger(harmfulTrigger)
+	log.Println("Attempting to connect to IRC network...")
 	mybot.Run()
 }
