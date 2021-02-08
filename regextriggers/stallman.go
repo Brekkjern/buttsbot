22088cfb6cbf12619d7e5733069038bd3bb33092
Nils Kristian Brekke <nkb@brekkjern.net>
2021-02-08 22:35:11 +0100

    Added WSL quote

--- regextriggers/stallman.go
+++ regextriggers/stallman.go
@@ -9,6 +9,8 @@ import (
 var stallmanRegex = regexp.MustCompile(`(?mi)Linux`)
 var stallmanResponses = []string{
 	"I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux.",
+	// Linux + Windows tweet: https://twitter.com/thatstupiddoll/status/1358789047565565953
+	"I'd just like to interject for a moment. What you're referring to as Linux, is in fact, Windows Subsystem for Linux, or as I've recently taken to calling it, WSL.\nLinux is not an operating system unto itself, but rather another free app in the Microsoft Store and part of a fully functioning Windows system made useful by the Windows OS, shell utilities and vital system components comprising a full OS as defined by POSIX.",
 }
 
 var StallmanTrigger = hbot.Trigger{
