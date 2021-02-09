1ebc451ec59a8bfbd0866e54fd97356d972b77d4
Brekkjern <gh@brekkjern.net>
2021-02-09 15:42:20 +0100

    Added systemd message to stallman trigger

--- regextriggers/stallman.go
+++ regextriggers/stallman.go
@@ -11,6 +11,7 @@ var stallmanResponses = []string{
 	"I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux.",
 	// Linux + Windows tweet: https://twitter.com/thatstupiddoll/status/1358789047565565953
 	"I'd just like to interject for a moment. What you're referring to as Linux, is in fact, Windows Subsystem for Linux, or as I've recently taken to calling it, WSL.\nLinux is not an operating system unto itself, but rather another free app in the Microsoft Store and part of a fully functioning Windows system made useful by the Windows OS, shell utilities and vital system components comprising a full OS as defined by POSIX.",
+	"I'd just like to interject for a moment. What you're referring to as Linux, is in fact, the Linux Subsystem for SystemD, or as I've recently taken to calling it, LSD. Linux is not an operating system unto itself, but rather another free component of fully functioning SystemD.",
 }
 
 var StallmanTrigger = hbot.Trigger{
