8a6c6854138df95c430c7bf6dc00df86708ca158
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2021-09-24 18:36:40 +0200

    Added 'devs' to developer trigger

--- regextriggers/developers.go
+++ regextriggers/developers.go
@@ -6,14 +6,14 @@ import (
 	hbot "github.com/whyrusleeping/hellabot"
 )
 
-var developersRegex = regexp.MustCompile("(?mi)developers|Ballmer")
+var developersRegex = regexp.MustCompile("(?mi)devs|developers|Ballmer")
 
 var DevelopersTrigger = hbot.Trigger{
 	Condition: func(b *hbot.Bot, m *hbot.Message) bool {
 		return standardizedRegexTrigger(b, m, developersRegex, 3)
 	},
 	Action: func(b *hbot.Bot, m *hbot.Message) bool {
-		b.Reply(m, "👏 developers 👏 developers 👏 developers 👏 developers 👏")
+		b.Reply(m, "👏 developers 👏 developers 👏 developers 👏")
 		return false
 	},
 }
