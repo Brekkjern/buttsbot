129c8302b2afca7ad9179aa69d0f351224caedc2
Nils Kristian Brekke <nkb@brekkjern.net>
2022-08-07 23:24:29 +0200

    Reduce developer frequency

--- regextriggers/developers.go
+++ regextriggers/developers.go
@@ -10,7 +10,7 @@ var developersRegex = regexp.MustCompile("(?mi)devs|developers|Ballmer")
 
 var DevelopersTrigger = hbot.Trigger{
 	Condition: func(b *hbot.Bot, m *hbot.Message) bool {
-		return standardizedRegexTrigger(b, m, developersRegex, 3)
+		return standardizedRegexTrigger(b, m, developersRegex, 7)
 	},
 	Action: func(b *hbot.Bot, m *hbot.Message) bool {
 		b.Reply(m, "👏 developers 👏 developers 👏 developers 👏")
