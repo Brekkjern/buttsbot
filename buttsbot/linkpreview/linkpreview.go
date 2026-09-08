6c0019434b7cee52713655f01337881da7d15525
Serguey Parkhomovsky <xindigo@gmail.com>
2026-09-08 13:18:21 -0700

    Revert "Revert twitter link preview to quip about Musk (#13)" (#14)
    This reverts commit 12eb38bc41b9a94ca1dc98b3faf074ecdd4a56e6.

--- buttsbot/linkpreview/linkpreview.go
+++ buttsbot/linkpreview/linkpreview.go
@@ -77,7 +77,7 @@ func linkPreviewAction(b *hbot.Bot, m *hbot.Message) bool {
 		case YouTube:
 			reply, err = previewYoutubeLink(pu)
 		case Twitter:
-			reply, err = previewTwitterLink()
+			reply, err = previewTwitterLink(pu)
 		case DefaultSite:
 			reply, err = previewDefaultLink(pu)
 		}
--- buttsbot/linkpreview/twitter.go
+++ buttsbot/linkpreview/twitter.go
@@ -1,5 +1,12 @@
 package linkpreview
 
-func previewTwitterLink() (string, error) {
-	return "Please pay Elon Musk $42,000 to preview this Twatter link.", nil
+import (
+	"net/url"
+)
+
+func previewTwitterLink(loc *url.URL) (string, error) {
+	xcancel := *loc
+	xcancel.Host = "xcancel.com"
+	xcancel.Scheme = "https"
+	return xcancel.String(), nil
 }
